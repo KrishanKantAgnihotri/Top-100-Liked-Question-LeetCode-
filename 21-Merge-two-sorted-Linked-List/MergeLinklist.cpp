214b0aef407f4caa44fb951ef55cc36c46533e0a
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-03-13 17:31:47 +0530

    Update MergeLinklist.cpp

--- 21-Merge-two-sorted-Linked-List/MergeLinklist.cpp
+++ 21-Merge-two-sorted-Linked-List/MergeLinklist.cpp
@@ -64,3 +64,54 @@ public:
         return ans;
     }
 };
+
+// without creating any extra space
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL){
+            return l2;
+        }
+        if(l2 == NULL){
+            return l1;
+        }
+        ListNode* cur = l1;
+        if(l1->val>l2->val){
+            cur = l2;
+            
+            l2 = l2->next;
+        }
+        else 
+            l1 = l1->next;
+        ListNode* ans = cur;
+        while(l1 && l2){
+            if(l1->val>l2->val){
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            else{
+                cur->next  = l1;
+                l1 = l1->next;
+            }
+            cur = cur->next;
+        }
+        if(l1){
+            cur->next =l1;
+        }
+        if(l2){
+            cur->next = l2;
+        }
+        return ans;
+        
+    }
+};
