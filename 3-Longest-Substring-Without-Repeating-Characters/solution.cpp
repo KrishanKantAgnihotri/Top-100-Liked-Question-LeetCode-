7891932219154e67ddc1558336c7c366b0a53de6
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-03-13 14:58:49 +0530

    Update solution.cpp

--- 3-Longest-Substring-Without-Repeating-Characters/solution.cpp
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.cpp
@@ -1,3 +1,4 @@
+//O(2n) with extra space 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -5,8 +6,8 @@ public:
         int i = 0 ;
         int j = 0 ; 
         int ans = 0 ; 
-        map<char,int> last;
-        map<char,int> vis;
+        unordered_map<char,int> last;
+        unordered_map<char,int> vis;
         for( ; j<n ;j++){
             if(vis[s[j]]){
                 int id = last[s[j]];
@@ -26,6 +27,25 @@ public:
         // cout<<endl;
         j--;
         ans = max(ans,j-i+1);
+        return ans;
+    }
+};
+//O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int i = 0 ;
+        int j = 0 ; 
+        int ans = 0 ; 
+        unordered_map<char,int> last;
+        for( ; j<n ;j++){
+            if(last.count(s[j])){
+                i = max(last[s[j]],i);
+            }
+            ans = max(ans,j-i+1);
+            last[s[j]] = j+1;
+        }
         return ans;
     }
 };
